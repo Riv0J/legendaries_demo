114ec2a80e43dbea4505ed66edc04572b3df272c
Rivo <107114580+Riv0J@users.noreply.github.com>
2025-03-12 11:41:41 +0100

    func banner

--- js/cards.js
+++ js/cards.js
@@ -1,24 +1,46 @@
 function q(sel){
     return document.querySelector(sel);
 }
-function create_cards() {
-    const container = q('#cards')
-    const template = q('#card_template')
-    
-    
-    creatures.forEach(creature => {
+function create_cards(category) {
+    const container = q('#cards');
+    const template = q('#card_template');
+
+    container.innerHTML = '';
+    console.log(category);
+
+    let delay = 0; // Para espaciar la aparición de cada carta
+
+    creatures.forEach((creature, index) => {
+        if (category && creature.category !== category) {
+            return;
+        }
+
         const params = {
-            'card-worn-number': 'card-worn-'+Math.floor((Math.random() * 4) + 1),
+            'card-worn-number': 'card-worn-' + Math.floor((Math.random() * 4) + 1),
             'card-type': creature.category == "beast" ? 'card-beast' : 'card-human',
             'card-period': creature.period == "ancient" ? 'card-ancient' : 'card-modern',
             'card-name': creature.name,
             'card-description': creature.title,
             'card-background': creature.img,
             'card-name-size': creature.name.length > 12 ? 'card-name-small' : 'card-name-normal',
-        }
-        container.innerHTML += replacer(template.innerHTML, params);
+        };
+
+        const card = document.createElement('div');
+        card.innerHTML = replacer(template.innerHTML, params);
+        // card.classList.add('card', 'hidden'); // Agrega una clase para la animación
+        container.appendChild(card);
+
+        // Aplica un retraso a la aparición de la carta
+        setTimeout(() => {
+            console.log(1);
+            
+            card.querySelector('.card').classList.add('visible');
+        }, delay);
+
+        delay += 100; // Aumenta el retraso para la siguiente carta
     });
 }
+
 
 function replacer(html, params){
     Object.keys(params).forEach(key => {
